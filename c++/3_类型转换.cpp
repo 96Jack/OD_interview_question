05e5f9f12829d66aaba6369c622535fe81f3fb6f
96Jack <xzw246850@163.com>
2023-04-25 10:16:39 +0800

    add data type of c++

--- c++/3_类型转换.cpp
+++ c++/3_类型转换.cpp
@@ -3,11 +3,30 @@
 
 using namespace std;
 int main(){
+    // 静态转换
+    // int a = 10;
+    // float b = static_cast<float>(a);
+    // cout << b << endl;
+    // cout << typeid(a).name()<< endl;
+    // cout << typeid(b).name() << endl;
+
+    // 动态转换
+    // class Base{};
+    // class   Derived : public Base{};
+    // Base* ptr_base = new Derived;
+    // Derived* ptr_derived = dynamic_cast<Derived*>(ptr_base);
+
+    // 常量转换
+    // const int a = 10;
+    // int& r = const_cast<int&>(a);
+    // cout << typeid(r).name() << endl;
+    // cout << typeid(a).name() << endl;
+
+    // 重新解释转换
     int a = 10;
-    float b = static_cast<float>(a);
-    cout << b << endl;
-    cout << typeid(a).name()<< endl;
+    float b = reinterpret_cast<float&>(a);
     cout << typeid(b).name() << endl;
+
     return 0;
 
 }
